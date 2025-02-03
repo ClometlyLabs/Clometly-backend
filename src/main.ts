97fddfbd259da4543c38e67d2a06c150f8e83fea
aquinoalejandro <aquinoalejandro666@gmail.com>
2025-02-03 06:25:57 -0300

    feat: :sparkles: Implement CORS middleware!

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 
+
 import * as dotenv from 'dotenv';
 
 import { AppModule } from './app.module';
@@ -8,6 +9,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // dotenv.config();
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT || 3000);
 }
